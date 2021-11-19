eeb4841a9b88bb265491be79131e0f5fa1b1b6ab
omeed06 <omeednr@gmail.com>
2021-11-19 14:41:24 +0500

    this is privacy policy page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
-
+import react from "react";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{marginLeft: "30px"}}>
+
+     <h3 style={{color: "#4A93C4"}}>PRIVACY POLICY</h3>
+
+     <p>360 App Services Inc. is dedicated to protect the privacy of our clients and visitors.
+       Here, we describe in details how 360 App Services Inc. will use and process your personal data.
+        Please note that by visiting, seeing and using 360appservices.com, you are accepting our policy,
+        terms & conditions and practices mentioned in this Privacy Policy Page.</p>
+       
+        <p>This Privacy Policy explains what happens to any personal information that you provide to us, or that we collect 
+          from you while you visit our website. Also, we do numerous update to this Policy from time to time, so please review
+           this Policy frequently.</p>
+         
+
+           <h3>INFORMATION THAT WE COLLECT FROM YOU:</h3>
+           <p>While visiting our website, we may collect and process the below mentioned information about you:</p>
+           <p>Why you are visiting our website? – Purpose about your use of our website include information of your visits like pages
+            viewed and the resources that you access. And, such information includes traffic data, communication data and location data. </p>
+       
+            <h3>Information given voluntarily by you:</h3>
+           <p> Data that you provide while you communicate with us.</p>
+        
+
+           <h5 style={{color: "#FB2821"}}> NOTE: We DO NOT save any payment details including your financial Details,
+            Credit Card OR Bank Account details while processing for any kind of payment.</h5>
+
+        
+
+            <h3> USE OF YOUR INFORMATION THAT WE COLLECT </h3>
+            <p>  We just use the data that we collect from your side to provide you our services. Additionally, we may use the information
+               for one or more of the below mentioned purposes:</p>
+
+          
+
+            <p> To quickly provide you the glance of information that you requested from us related to our products or services. To help you provide the
+               information related to other products that may be of interest to you. But such additional information will only be sent to you, if you have
+                consented to receive the information. And, to notify you of the changes to our website, services and products.</p>
+
+            <p> We may also send you the information and details of our products or services, if you have previously availed our other goods, products or services.
+               As that you may be interested in. And, if you have given your consent in advance, then only we may allow the selected third parties to use your information
+                so that they can provide you with the information regarding products and services, which we believe may interest you. But, if such consent has been provided by you earlier,
+               you can withdraw the consent anytime.</p> 
+
+               <h3> SAVING YOUR PERSONAL INFORMATION</h3>  
+               <p> By providing your personal information, you agree to this access. We do our level best to ensure that all the required steps are taken to ensure that your data is saved securely.
+                  Unfortunately, the information you are sending via the Internet is not fully secure and it can be intercepted easily. We cannot guarantee the data security that you send us electronically
+                 because sending such information is completely at your own risk.</p> 
+
+          
+
+            <h3>DISCLOSING YOUR DATA</h3>
+            <p>  We do not sell, trade, or rent End user’s personal information or identity information to any third parties. We may share the generic aggregated information, which is not related to any personal
+               identification information regarding visitors and users with our trusted affiliates, business partners and advertisers for the marketing purposes.</p>
+
+   
+
+                   <h3> IMBIBING THIRD PARTY LINKS OR WEBSITES</h3>
+                   <p> Sometimes, we may include links to the third parties on the website. And, even we may provide a link of third parties but that does not mean that we endorse or approve our site’s policy towards visitor privacy.
+                      Do review the third party privacy policy before sending the visitors any personal data.</p>
+
+
+
+
+                     <h3>AMENDMENTS TO THIS PRIVACY POLICY</h3>
+                   <p>Any time, we can update this privacy policy. And, when we do any amendments, we will definitely revise the updated date at the page. We encourage users to frequently check the page for any of the changes so that they can stay 
+                     informed about the steps we are taking to protect the personal information that we collect. Thus, we are considering that you acknowledge and agree that it’s solely your responsibility to check and review the privacy policy frequently 
+                     and periodically and become aware of the updates and modifications.</p>
+
+
+                   <h3>YOUR ACCEPTANCE TO THESE POLICY AND TERMS</h3>
+                   <p>If you are using our website, you signify your acceptance of this privacy policy. And, if you don’t agree to the policy, please do not use 360AppServices site. Your continued use of 360AppServices site following the sharing of changes to this privacy policy
+                      will be deemed as your acceptance of those updates and changes.</p>
+
     </div>
   );
 }
